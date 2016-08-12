53512ad9c3cbd091fe4ddb81e12155560ce7b453
anton5rov <anton5rov@gmail.com>
2016-08-12 14:05:06 +0300

    Indents fixed to 2 spaces and minor refractoring

--- src/github-browser.js
+++ src/github-browser.js
@@ -10,8 +10,7 @@ export class GitHubBrowser {
   
   heading = 'Github Browser';
   orgName = 'anton5rov-org'; //default repo changed for debug purposes
-  issues = {};
-
+  
   accessToken = null; // add your own access token here for higher API rate limits
 
   constructor(getHttpClient) {
@@ -40,31 +39,34 @@ export class GitHubBrowser {
 
     const response = await http.fetch(url);
     this.repositories = await response.json();
-	
-	this.getIssues(http);
+    
+    this.getIssues(http); // Call to added method
   }
   
+  // Addition start
+  issues = {};
+  
   async getIssues(http){
-	for (let repo of this.repositories){
-		var url = 'repos/' + this.orgName + '/' + repo.name + '/issues';
-		if(this.accessToken) {
-			url += '?access_token=' + this.accessToken;
-		}
-		
-		var repoIssues = await http.fetch(url);
-		var issuesJSON = await repoIssues.json();
-		var issuesCount = issuesJSON.length;
-		
-		var issuesObj = {cnt: 0, strng: ''};
-		
-		var issuesString = '';
-		for(let issue of issuesJSON){
-			issuesString += issue.number + '.: ' + 'title: '+ issue.title + ' body: ' + issue.body + '\n';
-		}
-		issuesObj.cnt = issuesCount;
-		issuesObj.strng = issuesString;
-		
-		this.issues[repo.name] = issuesObj;
-	}	
+  	for (let repo of this.repositories) {
+  		var url = 'repos/' + this.orgName + '/' + repo.name + '/issues';
+  		if(this.accessToken) {
+  			url += '?access_token=' + this.accessToken;
+  		}
+  		
+  		var repoIssues = await http.fetch(url);
+  		var issuesJSON = await repoIssues.json();
+  		var issuesCount = issuesJSON.length; // Issues per repo
+  		
+  		var issuesObj = {count: 0, text: ''};
+  		issuesObj.count = issuesCount;
+  		
+  		var issuesText = '';
+  		for (let issue of issuesJSON) {
+  		  issuesText += issue.number + '.: ' + 'title: '+ issue.title + ' body: ' + issue.body + '\n';
+  		}
+  		issuesObj.text = issuesText;
+  		
+  		this.issues[repo.name] = issuesObj;
+  	}	
   }
 }
